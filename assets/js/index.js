0dca47f6ded23d4f5cb776f21c95d4ae3f647c02
Kyrylo <fresh.js.ftd.@gmail.com>
2022-10-20 18:18:26 +0300

    Add setTimeout

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,12 @@
 const btn = document.querySelector('#btn');
+
+setTimeout(function callback() {
+  console.log('timeout');
+}, 0); 
+
+// console.time('loop timer');
+// for (let i = 0; i < 5000000000; i++) {
+//   let j = 0;
+//   j++;
+// }
+// console.timeEnd('loop timer');
