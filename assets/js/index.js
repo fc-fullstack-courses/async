ab7df4fd69dd04835c59b54a6649df1d6a0074c8
Kyrylo <fresh.js.ftd.@gmail.com>
2022-10-20 18:32:27 +0300

    Add clearTimeout

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,17 +1,33 @@
 const btn = document.querySelector('#btn');
 
-setTimeout(function callback() {
-  console.log('timeout 1');
-}, 100); // 3
-setTimeout(function callback() {
-  console.log('timeout 2');
-}, 500); // 4
-setTimeout(function callback() {
-  console.log('timeout 3');
-}, 20); // 2
-setTimeout(function callback() {
-  console.log('timeout 4');
-}, 0); // 1
+// setTimeout(function callback() {
+//   console.log('timeout 1');
+// }, 100); // 3
+// setTimeout(function callback() {
+//   console.log('timeout 2');
+// }, 500); // 4
+// setTimeout(function callback() {
+//   console.log('timeout 3');
+// }, 20); // 2
+// setTimeout(function callback() {
+//   console.log('timeout 4');
+// }, 0); // 1
+
+const id = setTimeout(
+  (value) => {
+    console.log(value);
+  },
+  0,
+  'test'
+);
+
+setTimeout(() => {
+  clearTimeout(id); // очищает таймаут
+}, 0);
+
+// setTimeout(() => {
+//   console.log('long');
+// }, 20000);
 
 // console.time('loop timer');
 // for (let i = 0; i < 5000000000; i++) {
