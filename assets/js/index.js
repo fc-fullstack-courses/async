6758020c96f2a42e923c9c6a2389728b781fa0a0
Kyrylo <fresh.js.ftd.@gmail.com>
2022-10-20 18:22:01 +0300

    Add priority of multiple timeouts

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,17 @@
 const btn = document.querySelector('#btn');
 
 setTimeout(function callback() {
-  console.log('timeout');
-}, 0); 
+  console.log('timeout 1');
+}, 100); // 3
+setTimeout(function callback() {
+  console.log('timeout 2');
+}, 500); // 4
+setTimeout(function callback() {
+  console.log('timeout 3');
+}, 20); // 2
+setTimeout(function callback() {
+  console.log('timeout 4');
+}, 0); // 1
 
 // console.time('loop timer');
 // for (let i = 0; i < 5000000000; i++) {
